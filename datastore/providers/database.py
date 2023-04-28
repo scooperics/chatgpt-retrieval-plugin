16cabb9875ed58a8c4fc8424334fea3ca6f00af8
Stephen Cooper <scooper@intuitivecloudsolutions.com>
2023-04-28 08:01:43 -0400

    fix: removed hack to ingore limit on 8K... now that the model prioritized earnings transcripts and 10-Ks... not required

--- datastore/providers/database.py
+++ datastore/providers/database.py
@@ -51,7 +51,7 @@ def lookup_documents(sort_order, limit, symbol, form_types, fiscal_quarter, fisc
             params.append(str(fiscal_year))
  
         # Apply sorting and limiting if all three parameters are not None
-        if sort_order is not None and limit is not None and form_types != ["8-K"]:
+        if sort_order is not None and limit is not None:
             query += f" ORDER BY published_date {sort_order} LIMIT %s"
             params.append(limit)
 
